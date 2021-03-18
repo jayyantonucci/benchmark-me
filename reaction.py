246429a8fe30d12b5c7fc3a9e669841d993f5d93
jayyantonucci <75542433+jayyantonucci@users.noreply.github.com>
2021-03-17 20:52:47 -0400

    basic reaction test with average

--- reaction.py
+++ reaction.py
@@ -1,20 +1,32 @@
+import os
 import msvcrt as m
 import time, random
 
-def wait():
+os.system("cls")
+
+
+def reaction():
+    print('Reaction test\n')
+    react_times = []
+    print('any key to start, PRESS ENTER after random delay\n')
     m.getch()
+    
+    for i in range(3):
+        print('...get ready...')
+        time.sleep(random.randint(2,5))
+        print('PRESS ENTER')
+        time_start = time.time()
+        input()
+        time_end = time.time()
+        time_react = time_end - time_start
+        if time_react < 0.01:
+            print('too soon junior...')
+            quit()
+        print(f'time: {time_react:.3f} seconds\n\n')
+        react_times.append(time_react)
 
-print('any key to start, press enter after random delay')
-wait()
+    print(react_times)
+    react_avg = sum(react_times)/len(react_times)
+    print(f'Average time: {react_avg:.3f} seconds')
 
-print('\n...get ready...\n')
-
-time.sleep(random.randint(2,5))
-print('PRESS ENTER')
-time_start = time.time()
-input()
-time_end = time.time()
-time_react = time_end - time_start
-print('time ' + str(time_react) + ' seconds')
-
-
+reaction()
